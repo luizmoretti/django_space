35fc46e8857ed0464090c8f414f82320d3a0dc2b
luizmoretti <118682075+luizmoretti@users.noreply.github.com>
2023-09-06 19:12:33 -0400

    Testes de Codigos

--- galeria/views.py
+++ galeria/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
 
-
-
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+    1: {"Nome": "Nebulosa de Carina",
+        "legenda": "webtelescop.org / NASA / James Webb"},
+    
+    2: {"Nome": "Galaxia  NGC 1079",
+        "legenda": "nasa.org / NASA / Hublle"},
+}
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
