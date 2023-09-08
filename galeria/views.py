b10597a19bd13332a41c133bf4e18b90faad0b85
luizmoretti <118682075+luizmoretti@users.noreply.github.com>
2023-09-08 13:08:03 -0400

    Update de codigo

--- galeria/views.py
+++ galeria/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 
 def index(request):
     dados = {
-    1: {"Nome": "Nebulosa de Carina",
+    1: {"nome": "Nebulosa de Carina",
         "legenda": "webtelescop.org / NASA / James Webb"},
     
-    2: {"Nome": "Galaxia  NGC 1079",
+    2: {"nome": "Galaxia  NGC 1079",
         "legenda": "nasa.org / NASA / Hublle"},
-}
+    }
     return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
