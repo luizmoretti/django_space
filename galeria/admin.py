8f8ca8756db9034394a3421a4c39895a5c8521e1
luizmoretti <118682075+luizmoretti@users.noreply.github.com>
2023-09-10 15:38:16 -0400

    Configurando pagina admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from galeria.models import Fotos
+
+class ListFotos(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda', 'foto')
+    list_display_links = ('id', 'nome')
+    search_fields = ("nome",)
+
+admin.site.register(Fotos, ListFotos)
