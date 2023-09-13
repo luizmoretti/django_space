6ff5267387a751d27c05acfc991f75de35b6d0ac
luizmoretti <118682075+luizmoretti@users.noreply.github.com>
2023-09-13 16:53:59 -0400

    Update DB

--- galeria/models.py
+++ galeria/models.py
@@ -2,8 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Fotos(models.Model):
+
+    opcoes_categoria = [
+        ("NEBULOSA", "Nebulosa"),
+        ("ESTRELA", "Estrela"),
+        ("GALAXIA", "Galáxia"),
+        ("PLANETA", "Planeta"),
+    ]
+
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=opcoes_categoria,  default='')
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
     
